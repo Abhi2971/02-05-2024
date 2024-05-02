d02aa9fafc6426739ffe9cb663f12322b66ee7f8
Abhishek <abhishelke297127@gmail.com>
2024-05-02 10:34:11 +0530

    classes

--- a1.js
+++ a1.js
@@ -1,33 +1,73 @@
-// // set
-// const oddNum = new set([1,3,5,7,9,13]);
-// console.log(oddNum);
+// // // set
+// // const oddNum = new set([1,3,5,7,9,13]);
+// // console.log(oddNum);
 
-// let a = oddNum.add(19);
-// console.log(a);
+// // let a = oddNum.add(19);
+// // console.log(a);
 
-// // oddNum.clear();
-// console.log(oddNum);
+// // // oddNum.clear();
+// // console.log(oddNum);
 
-// let b = oddNum.delete(19);
-// console.log(b);
+// // let b = oddNum.delete(19);
+// // console.log(b);
 
-// let c = oddNum.has(17);
-// console.log(c);
+// // let c = oddNum.has(17);
+// // console.log(c);
 
-// map
-const frd= new Map([
-    ["Abhishek","shelke"],
-    ["Ganesh","pawar"]
-]);
+// // map
+// const frd= new Map([
+//     ["Abhishek","shelke"],
+//     ["Ganesh","pawar"]
+// ]);
 
-console.log(frd.delete("Abhishek"));
+// console.log(frd.delete("Abhishek"));
 
-console.log(frd.get("Abhishek"));
+// console.log(frd.get("Abhishek"));
 
-console.log(frd.has("ganesh"));// It is an case censative 
-console.log(frd.has("Ganesh"));
-console.log(frd.size);
+// console.log(frd.has("ganesh"));// It is an case censative 
+// console.log(frd.has("Ganesh"));
+// console.log(frd.size);
 //****************************************** how to create OBJECTS/CLASSES ****************************************************************
 
+class animal{
+    // #color;  it is private 
+    color;
+    height;
+    weight;
+    food;
+    #hairColor;
+    constructor(color,height,weight,food,hairColor){
+        this.color = color;
+        this.food = food;
+        this.weight = weight;
+        this.height = height;
+        this.#hairColor=hairColor;
 
+    }
+    eats(){
 
+    }
+    sleep(){
+
+    }
+    running(){
+
+    }
+    set hairColor(hairColor){
+        this.#hairColor=hairColor; // this set value of function which acess the values which deriverd from outside the class
+
+    }
+    get hairColor(){
+        return this.#hairColor;
+    } // private member access. It covert to public from private.
+}
+
+let myCat = new animal("red",10,10,"mouse","black");
+
+console.log(myCat.color);
+myCat.eats();
+// setter
+console.log(myCat.hairColor);
+// getter
+myCat.hairColor = "pink";
+console.log(myCat.hairColor);
