5091abe5b227593ff4d7dbbfbbff7cc88cef8051
Abhishek <abhishelke297127@gmail.com>
2024-05-02 10:02:25 +0530

    see/map

---
+++ a1.js
@@ -0,0 +1,33 @@
+// // set
+// const oddNum = new set([1,3,5,7,9,13]);
+// console.log(oddNum);
+
+// let a = oddNum.add(19);
+// console.log(a);
+
+// // oddNum.clear();
+// console.log(oddNum);
+
+// let b = oddNum.delete(19);
+// console.log(b);
+
+// let c = oddNum.has(17);
+// console.log(c);
+
+// map
+const frd= new Map([
+    ["Abhishek","shelke"],
+    ["Ganesh","pawar"]
+]);
+
+console.log(frd.delete("Abhishek"));
+
+console.log(frd.get("Abhishek"));
+
+console.log(frd.has("ganesh"));// It is an case censative 
+console.log(frd.has("Ganesh"));
+console.log(frd.size);
+//****************************************** how to create OBJECTS/CLASSES ****************************************************************
+
+
+
